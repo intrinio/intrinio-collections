b01ea763df308768b0f0febf161839adad61ca03
Shawn Snyder <ssnyder@intrinio.com>
2025-09-05 19:19:06 -0500

    RingBuffer<T>

--- Intrinio.Collections/RingBuffers/RingBuffer.cs
+++ Intrinio.Collections/RingBuffers/RingBuffer.cs
@@ -136,3 +136,136 @@ public class RingBuffer : IRingBuffer
         return Interlocked.Read(ref _count) == 0UL;
     }
 }
+
+/// <summary>
+/// A thread-safe implementation of the IRingBuffer (multiple producer and multiple consumer).  Full behavior: the block trying to be enqueued will be dropped. 
+/// </summary>
+public class RingBuffer<T> : IRingBuffer<T> where T : struct
+{
+    #region Data Members
+    private readonly T[] _data;
+    private readonly T DEFAULT = default(T);
+    private ulong _nextReadIndex;
+    private ulong _nextWriteIndex;
+#if NET9_0_OR_GREATER
+    private readonly Lock _readLock;
+    private readonly Lock _writeLock;
+#else
+    private readonly object _readLock;
+    private readonly object _writeLock;
+#endif
+    private ulong _count;
+    private readonly ulong _capacity;
+    private ulong _dropCount;
+    private ulong _processed;
+    public ulong ProcessedCount { get { return Interlocked.Read(ref _processed); } }
+    
+    public ulong Count { get { return Interlocked.Read(ref _count); } }
+    public ulong Capacity { get { return _capacity; } }
+    public ulong DropCount { get { return Interlocked.Read(ref _dropCount); } }
+
+    public bool IsEmpty
+    {
+        get
+        {
+            return IsEmptyNoLock();
+        }
+    }
+
+    public bool IsFull
+    {
+        get
+        {
+            return IsFullNoLock();
+        }
+    }
+    #endregion //Data Members
+
+    #region Constructors
+
+    /// <summary>
+    /// A thread-safe implementation of a circular buffer (multiple producer and multiple consumer).  Full behavior: the <see cref="T"/> trying to be enqueued will be dropped. 
+    /// </summary>
+    /// <param name="capacity">The fixed capacity.</param>
+    public RingBuffer(ulong capacity)
+    {
+        this._capacity = capacity;
+        _processed = 0UL;
+        _nextReadIndex = 0u;
+        _nextWriteIndex = 0u;
+        _count = 0u;
+        _dropCount = 0UL;
+        _readLock = new();
+        _writeLock = new();
+        _data = new T[capacity];
+    }
+
+    #endregion //Constructors
+
+    /// <summary>
+    /// Thread-safe try enqueue.
+    /// Full behavior: the <see cref="T"/> trying to be enqueued will be dropped. 
+    /// </summary>
+    /// <returns>Whether the enqueue was successful or not.</returns>
+    /// <param name="obj">The <see cref="T"/> to enqueue.</param>
+    public bool TryEnqueue(T obj)
+    {
+        if (IsFullNoLock())
+        {
+            Interlocked.Increment(ref _dropCount);
+            return false;
+        }
+
+        lock (_writeLock)
+        {
+            if (IsFullNoLock())
+            {
+                Interlocked.Increment(ref _dropCount);
+                return false;
+            }
+            
+            _data[_nextWriteIndex] = obj;
+            
+            _nextWriteIndex = (++_nextWriteIndex) % Capacity;
+            Interlocked.Increment(ref _count);
+
+            return true;
+        }
+    }
+
+    /// <summary>
+    /// Thread-safe try dequeue.
+    /// </summary>
+    /// <returns>Whether the dequeue was successful or not.</returns>
+    /// <param name="obj">The dequeued <see cref="T"/>.</param>
+    public bool TryDequeue(out T obj)
+    {
+        lock (_readLock)
+        {
+            if (IsEmptyNoLock())
+            {
+                obj = DEFAULT;
+                return false;
+            }
+
+            obj = _data[_nextReadIndex];
+            
+            _nextReadIndex = (++_nextReadIndex) % Capacity;
+            Interlocked.Decrement(ref _count);
+            Interlocked.Increment(ref _processed);
+            return true;
+        }
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private bool IsFullNoLock()
+    {
+        return Interlocked.Read(ref _count) == _capacity;
+    }
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private bool IsEmptyNoLock()
+    {
+        return Interlocked.Read(ref _count) == 0UL;
+    }
+}
--- Intrinio.Collections/RingBuffers/SingleProducerRingBuffer.cs
+++ Intrinio.Collections/RingBuffers/SingleProducerRingBuffer.cs
@@ -124,3 +124,124 @@ public class SingleProducerRingBuffer : IRingBuffer
         return Interlocked.Read(ref _count) == 0UL;
     }
 }
+
+/// <summary>
+/// A read thread-safe, write not thread-safe implementation of the IRingBuffer (single producer and multiple consumer).  Full behavior: the block trying to be enqueued will be dropped. 
+/// </summary>
+public class SingleProducerRingBuffer<T> : IRingBuffer<T> where T : struct
+{
+    #region Data Members
+    private readonly T[] _data;
+    private readonly T DEFAULT = default(T);
+    private ulong _nextReadIndex;
+    private ulong _nextWriteIndex;
+#if NET9_0_OR_GREATER
+    private readonly Lock _readLock;
+#else
+    private readonly object _readLock;
+#endif
+    private ulong _count;
+    private readonly ulong _capacity;
+    private ulong _dropCount;
+    private ulong _processed;
+    public ulong ProcessedCount { get { return Interlocked.Read(ref _processed); } }
+
+    public ulong Count { get { return Interlocked.Read(ref _count); } }
+    public ulong Capacity { get { return _capacity; } }
+    public ulong DropCount { get { return Interlocked.Read(ref _dropCount); } }
+
+    public bool IsEmpty
+    {
+        get
+        {
+            return IsEmptyNoLock();
+        }
+    }
+
+    public bool IsFull
+    {
+        get
+        {
+            return IsFullNoLock();
+        }
+    }
+    #endregion //Data Members
+
+    #region Constructors
+
+    /// <summary>
+    /// A read thread-safe, write not thread-safe implementation of the IRingBuffer (single producer and multiple consumer).  Full behavior: the block trying to be enqueued will be dropped. 
+    /// </summary>
+    /// <param name="capacity">The fixed capacity of <see cref="T"/> count.</param>
+    public SingleProducerRingBuffer(ulong capacity)
+    {
+        this._capacity = capacity;
+        _processed = 0UL;
+        _nextReadIndex = 0u;
+        _nextWriteIndex = 0u;
+        _count = 0u;
+        _dropCount = 0UL;
+        _readLock = new();
+        _data = new T[capacity];
+    }
+
+    #endregion //Constructors
+
+    /// <summary>
+    /// Not thread-safe try enqueue.  This is not safe for calling concurrently, and intended for use with a single producer.
+    /// Full behavior: the <see cref="T"/> trying to be enqueued will be dropped. 
+    /// </summary>
+    /// <returns>Whether the enqueue was successful or not.</returns>
+    /// <param name="obj">The <see cref="T"/> to enqueue.</param>
+    public bool TryEnqueue(T obj)
+    {
+        if (IsFullNoLock())
+        {
+            Interlocked.Increment(ref _dropCount);
+            return false;
+        }
+            
+        _data[_nextWriteIndex] = obj;
+            
+        _nextWriteIndex = (++_nextWriteIndex) % Capacity;
+        Interlocked.Increment(ref _count);
+
+        return true;
+    }
+
+    /// <summary>
+    /// Thread-safe try dequeue.
+    /// </summary>
+    /// <returns>Whether the dequeue was successful or not.</returns>
+    /// <param name="obj">The dequeued <see cref="T"/>.</param>
+    public bool TryDequeue(out T obj)
+    {
+        lock (_readLock)
+        {
+            if (IsEmptyNoLock())
+            {
+                obj = DEFAULT;
+                return false;
+            }
+
+            obj = _data[_nextReadIndex];
+            
+            _nextReadIndex = (++_nextReadIndex) % Capacity;
+            Interlocked.Decrement(ref _count);
+            Interlocked.Increment(ref _processed);
+            return true;
+        }
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private bool IsFullNoLock()
+    {
+        return Interlocked.Read(ref _count) == _capacity;
+    }
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private bool IsEmptyNoLock()
+    {
+        return Interlocked.Read(ref _count) == 0UL;
+    }
+}
