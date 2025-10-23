ce6e59ae4d2113b07aeed937ea8779de34b8a931
ssnyder-intrinio <87659980+ssnyder-intrinio@users.noreply.github.com>
2025-10-22 21:31:50 -0500

    Dynamic Block Priority RingBuffer Pool (#15)
    * DynamicBlockPriorityRingBufferPool
    
    * Version bump

---
+++ Intrinio.Collections/RingBuffers/DynamicBlockPriorityRingBufferPool.cs
@@ -0,0 +1,190 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Intrinio.Collections.RingBuffers;
+
+public class DynamicBlockPriorityRingBufferPool : IDynamicBlockPriorityRingBufferPool
+{
+    private readonly uint _blockSize;
+    private readonly ulong _blockCapacity;
+    private readonly List<IDynamicBlockRingBuffer?> _ringBuffers;
+    
+#if NET9_0_OR_GREATER
+    private readonly Lock               _addUpdateLock = new Lock();
+#else
+    private readonly object             _addUpdateLock = new object();
+#endif
+
+    public DynamicBlockPriorityRingBufferPool(uint blockSize, ulong blockCapacity)
+    {
+        _blockSize     = blockSize;
+        _blockCapacity = blockCapacity;
+        _ringBuffers   = new List<IDynamicBlockRingBuffer?>();
+    }
+
+    public void AddUpdateRingBufferToPool(uint priority, IDynamicBlockRingBuffer ringBuffer)
+    {
+        if (ringBuffer.BlockSize != _blockSize)
+            throw new ArgumentException("RingBuffer BlockSize must match the pool BlockSize");
+        
+        lock (_addUpdateLock)
+        {
+            while (_ringBuffers.Count <= priority) //We need to expand
+                _ringBuffers.Add(null);
+
+            _ringBuffers[Convert.ToInt32(priority)] = ringBuffer;
+        }
+    }
+
+    public bool TryEnqueue(uint priority, ReadOnlySpan<byte> blockToWrite)
+    {
+        if (_ringBuffers.Count <= priority || _ringBuffers[Convert.ToInt32(priority)] == null)
+            return false;
+
+        return _ringBuffers[Convert.ToInt32(priority)].TryEnqueue(blockToWrite);
+    }
+
+    public bool TryDequeue(Span<byte> blockBuffer)
+    {
+        int i = 0;
+        while (i < _ringBuffers.Count)
+        {
+            if (_ringBuffers[i] != null && _ringBuffers[i].TryDequeue(blockBuffer))
+                return true;
+            
+            ++i;
+        }
+
+        return false;
+    }
+    
+    public bool TryDequeue(Span<byte> fullBlockBuffer, out Span<byte> trimmedBuffer)
+    {
+        int i = 0;
+        while (i < _ringBuffers.Count)
+        {
+            if (_ringBuffers[i] != null && _ringBuffers[i].TryDequeue(fullBlockBuffer, out trimmedBuffer))
+                return true;
+            
+            ++i;
+        }
+
+        trimmedBuffer = fullBlockBuffer;
+        return false;
+    }
+
+    public ulong Count
+    {
+        get
+        {
+            ulong total = 0UL;
+            for (int i = 0; i < _ringBuffers.Count; i++)
+            {
+                if (_ringBuffers[i] != null)
+                    total += _ringBuffers[i].Count;
+            }
+
+            return total;
+        }
+    }
+
+    public ulong ProcessedCount
+    {
+        get
+        {
+            ulong total = 0UL;
+            for (int i = 0; i < _ringBuffers.Count; i++)
+            {
+                if (_ringBuffers[i] != null)
+                    total += _ringBuffers[i].ProcessedCount;
+            }
+
+            return total;
+        }
+    }
+
+    public uint BlockSize
+    {
+        get { return _blockSize; }
+    }
+
+    public ulong EachQueueBlockCapacity
+    {
+        get { return _blockCapacity; }
+    }
+
+    public ulong TotalBlockCapacity
+    {
+        get
+        {
+            ulong total = 0UL;
+            for (int i = 0; i < _ringBuffers.Count; i++)
+            {
+                if (_ringBuffers[i] != null)
+                    total += _ringBuffers[i].BlockCapacity;
+            }
+
+            return total;
+        }
+    }
+
+    public ulong DropCount
+    {
+        get
+        {
+            ulong total = 0UL;
+            for (int i = 0; i < _ringBuffers.Count; i++)
+            {
+                if (_ringBuffers[i] != null)
+                    total += _ringBuffers[i].DropCount;
+            }
+
+            return total;
+        }
+    }
+
+    public bool IsEmpty
+    {
+        get
+        {
+            for (int i = 0; i < _ringBuffers.Count; i++)
+            {
+                if (_ringBuffers[i] != null && !_ringBuffers[i].IsEmpty)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+
+    public bool IsFull
+    {
+        get
+        {
+            for (int i = 0; i < _ringBuffers.Count; i++)
+            {
+                if (_ringBuffers[i] != null && !_ringBuffers[i].IsFull)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+
+    public ulong GetCount(uint priority)
+    {
+        if (_ringBuffers.Count <= priority || _ringBuffers[Convert.ToInt32(priority)] == null)
+            return 0UL;
+
+        return _ringBuffers[Convert.ToInt32(priority)].Count;
+    }
+
+    public ulong GetDropCount(uint priority)
+    {
+        if (_ringBuffers.Count <= priority || _ringBuffers[Convert.ToInt32(priority)] == null)
+            return 0UL;
+
+        return _ringBuffers[Convert.ToInt32(priority)].DropCount;
+    }
+}
---
+++ Intrinio.Collections/RingBuffers/IDynamicBlockPriorityRingBufferPool.cs
@@ -0,0 +1,93 @@
+namespace Intrinio.Collections.RingBuffers;
+
+using System;
+
+/// <summary>
+/// A pool of <see cref="Intrinio.Collections.RingBuffers.IRingBuffer"/>, each handing a specific priority. Priorities should be numerically compact.
+/// </summary>
+public interface IDynamicBlockPriorityRingBufferPool
+{
+    /// <summary>
+    /// Add/Update a <see cref="Intrinio.Collections.RingBuffers.IRingBuffer"/> to the pool at the provided priority.
+    /// </summary>
+    /// <param name="priority"></param>
+    /// <param name="ringBuffer"></param>
+    void AddUpdateRingBufferToPool(uint priority, IDynamicBlockRingBuffer ringBuffer);
+    
+    /// <summary>
+    /// Try to enqueue in the <see cref="Intrinio.Collections.RingBuffers.IRingBuffer"/> corresponding with the provided priority.
+    /// </summary>
+    /// <param name="priority">The zero based priority.</param>
+    /// <param name="blockToWrite">The byte block to copy from.</param>
+    /// <returns>Whether the block was successfully enqueued or not.</returns>
+    bool TryEnqueue(uint priority, ReadOnlySpan<byte> blockToWrite);
+    
+    /// <summary>
+    /// Try to dequeue.  Parameter "blockBuffer" MUST be of length BlockSize or greater! Dequeues from the ascending highest priority queue first. 
+    /// </summary>
+    /// <param name="blockBuffer">The buffer to copy the byte block to.</param>
+    /// <returns>Whether a block was successfully dequeued or not.</returns>
+    bool TryDequeue(Span<byte> blockBuffer);
+    
+    /// <summary>
+    /// Try to dequeue a byte block via copy to the provided buffer. Dequeues from the ascending highest priority queue first.
+    /// </summary>
+    /// <param name="fullBlockBuffer">The full sized buffer to copy the byte block to.</param>
+    /// <param name="trimmedBuffer">The fullBlockBuffer, trimmed down to the original size it enqueued as.</param>
+    /// <returns>Whether the dequeue successfully retrieved a block or not.</returns>
+    bool TryDequeue(Span<byte> fullBlockBuffer, out Span<byte> trimmedBuffer);
+    
+    /// <summary>
+    /// The count of blocks currently in the ring buffer.
+    /// </summary>
+    ulong Count { get; }
+    
+    /// <summary>
+    /// The quantity of blocks dequeued from the ring buffer.
+    /// </summary>
+    ulong ProcessedCount { get ; }
+    
+    /// <summary>
+    /// The fixed size of each byte block.
+    /// </summary>
+    uint BlockSize { get; }
+    
+    /// <summary>
+    /// The fixed capacity of blocks in each ring buffer.
+    /// </summary>
+    ulong EachQueueBlockCapacity { get; }
+    
+    /// <summary>
+    /// The fixed total capacity of blocks across all ring buffers.
+    /// </summary>
+    ulong TotalBlockCapacity { get; }
+    
+    /// <summary>
+    /// The quantity of dropped blocks due to being full. 
+    /// </summary>
+    ulong DropCount { get; }
+    
+    /// <summary>
+    /// Whether the ring buffer is currently empty.
+    /// </summary>
+    bool IsEmpty { get; }
+    
+    /// <summary>
+    /// Whether the ring buffer is currently full.
+    /// </summary>
+    bool IsFull { get; }
+
+    /// <summary>
+    /// The count of blocks currently in the ring buffer at the specified priority.
+    /// </summary>
+    /// <param name="priority">The zero based priority to check.</param>
+    /// <returns></returns>
+    public ulong GetCount(uint priority);
+
+    /// <summary>
+    /// The quantity of dropped blocks at the specified priority.
+    /// </summary>
+    /// <param name="priority">The zero based priority to check.</param>
+    /// <returns></returns>
+    public ulong GetDropCount(uint priority);
+}
